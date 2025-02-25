1bd5bd84ebf1ba447436b038b3512d5e3ac6a978
EmiliaCardenas <a01706807@tec.mx>
2025-02-25 10:36:50 -0600

    body parsels

--- Laboratorio 11/app.js
+++ Laboratorio 11/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use((request, response, next) => {
@@ -9,8 +12,10 @@ app.use((request, response, next) => {
 
 //Solo en la ruta SKZ
 app.use('/SKZ',(request, response, next) => {
+    console.log(request.body);
     response.send("Holiwis desde una ruta /SKZ")
 });
+
 
 app.use((request, response, next) => {
     console.log('Otro middleware!');
