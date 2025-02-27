bbd8c6aca17e539cc527352ed992761ec5494a35
EmiliaCardenas <a01706807@tec.mx>
2025-02-27 10:14:17 -0600

    EJS

--- Laboratorio 11/app.js
+++ Laboratorio 11/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+
+const path = requite('path');
+
+path.use(express.static(path.join(__dirname,'Public')));
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
