d33ae54a58754beace91cb4bc1a02e139aad2b3d
EmiliaCardenas <a01706807@tec.mx>
2025-03-16 09:31:51 -0600

    Laboratorio 19 terminado

--- Laboratorio 11/kpop_db.sql
+++ Laboratorio 11/kpop_db.sql
@@ -2,8 +2,35 @@ CREATE DATABASE kpop_db;
 
 USE kpop_db;
 
-CREATE TABLE idols (
+CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    grupo VARCHAR(100) NOT NULL
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE permisos (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE usuario_roles (
+    usuario_id INT,
+    rol_id INT,
+    PRIMARY KEY (usuario_id, rol_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    FOREIGN KEY (rol_id) REFERENCES roles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE rol_permisos (
+    rol_id INT,
+    permiso_id INT,
+    PRIMARY KEY (rol_id, permiso_id),
+    FOREIGN KEY (rol_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (permiso_id) REFERENCES permisos(id) ON DELETE CASCADE
+);
+
